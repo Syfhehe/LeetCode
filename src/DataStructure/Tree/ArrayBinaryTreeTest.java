95bf1eb7dbf2065ccbc564c4023879368fcd4dac
Syfhehe <shenyf0811@gmail.com>
2021-03-26 20:46:11 +0800

    顺序存储二叉树

---
+++ src/DataStructure/Tree/ArrayBinaryTreeTest.java
@@ -0,0 +1,11 @@
+package DataStructure.Tree;
+
+public class ArrayBinaryTreeTest {
+
+  public static void main(String[] args) {
+    int[] data = {1, 2, 3, 4, 5, 6, 7};
+    ArrayBinaryTree tree = new ArrayBinaryTree(data);
+    tree.frontShow();
+  }
+
+}
